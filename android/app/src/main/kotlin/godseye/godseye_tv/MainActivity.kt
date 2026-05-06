4ff34f0d0748d103142fb57a3b65ca006af4c4ef
localhostdeveloper <localhostdevelop9@gmail.com>
2026-05-06 01:01:25 +0000

    first commit

---
+++ android/app/src/main/kotlin/godseye/godseye_tv/MainActivity.kt
@@ -0,0 +1,5 @@
+package godseye.godseye_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryRed = Color(0xFFE50914);
+  static const Color backgroundBlack = Color(0xFF0D0D1D); // Dark navy/black from your palette
+  static const Color surfaceDark = Color(0xFF1A1A2E);    // For cards and navigation bars
+  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color textGrey = Color(0xFF9E9E9E);
+}
